69c1453a214601fd4f06428f0153efd55c51ac75
HarshVathare <harsh1234vathare@gmail.com>
2026-03-16 12:18:41 +0530

    add activity controller Api's

---
+++ src/main/java/com/project/fitness/DTO/ActivityRequest.java
@@ -0,0 +1,9 @@
+package com.project.fitness.DTO;
+
+import java.util.Map;
+
+public class ActivityRequest {
+    private String activityType;
+    private Map<String , Object> additional_matrics;
+    private String
+}
---
+++ src/main/java/com/project/fitness/DTO/ActivityResponse.java
@@ -0,0 +1,4 @@
+package com.project.fitness.DTO;
+
+public class ActivityResponse {
+}
