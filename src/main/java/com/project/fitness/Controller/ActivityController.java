757054750114ee77b18233c2a3765220fe1b532b
HarshVathare <harsh1234vathare@gmail.com>
2026-04-01 19:57:20 +0530

    Add Docker-compose file properly

--- src/main/java/com/project/fitness/Controller/ActivityController.java
+++ src/main/java/com/project/fitness/Controller/ActivityController.java
@@ -11,10 +11,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/activities")
-@RequiredArgsConstructor
 public class ActivityController {
 
     private final ActivityService activityService;
+
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
 
     @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest createActivityRequest) {
